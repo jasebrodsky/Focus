c2c7f29877db83fd52abed03b577efca3ca6b60b
Jason Brodsky-Porges <jbrod@Jasons-MacBook-Pro.local>
2020-03-01 19:17:38 -0500

    forced dark mode off so that datepicker renders correctly

--- android/app/src/main/java/com/focus/MainApplication.java
+++ android/app/src/main/java/com/focus/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import io.expo.appearance.RNCAppearancePackage;
 import com.microsoft.codepush.react.CodePush;
 import com.bluroverly.SajjadBlurOverlayPackage;
 import com.reactnativecommunity.geolocation.GeolocationPackage;
