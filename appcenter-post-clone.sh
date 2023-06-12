ad7f696afde169df67d7abf6dfb313a851b9baa1
Jason Brodsky <jasebrodsky@gmail.com>
2023-06-11 21:00:52 -0400

    test

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -3,9 +3,9 @@ CUR_COCOAPODS_VER=`sed -n -e 's/^COCOAPODS: \([0-9.]*\)/\1/p' ios/Podfile.lock`
 ENV_COCOAPODS_VER=`pod --version`
 
 
-touch .npmrc
-echo "always-auth=true
-registry=https://github.com/EvanBacon/react-native-parsed-text/:_authToken=${READ_TOKEN}" > .npmrc
+#touch .npmrc
+#echo "always-auth=true
+#registry=https://github.com/EvanBacon/react-native-parsed-text/:_authToken=${READ_TOKEN}" > .npmrc
 
 # check if not the same version, reinstall cocoapods version to current project's
 if [ $CUR_COCOAPODS_VER != $ENV_COCOAPODS_VER ];
